93fe21fdbc6b01e41ca8571cc06ea99dd1fedfe8
Aleksandr-Borodin <aleksandrov477@gmail.com>
2018-07-25 14:40:34 +0300

    Исправлена ошибка с адресом класса автозагрузки классов

--- input-data.php
+++ input-data.php
@@ -1,6 +1,6 @@
 <?php
 // Автозагрузка классво через ооп;
-$path = __DIR__ . '\load.php';
+$path = __DIR__ . '\classes\load.php';
 require_once $path;
 $loader = new Load();
 spl_autoload_register([$loader, 'loadclass']);
