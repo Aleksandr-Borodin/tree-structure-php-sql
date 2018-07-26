157afe4b73c8e4543a56142504597190251e1e29
Aleksandr-Borodin <aleksandrov477@gmail.com>
2018-07-27 00:03:00 +0300

    Заменен слеш

--- classes/Load.php
+++ classes/Load.php
@@ -4,7 +4,7 @@ class Load
 {
     public function loadclass($a)
     {
-        $b = __DIR__ . '\\' . $a . '.php';
+        $b = __DIR__ . '//' . $a . '.php';
         if (!file_exists($b)) {
             echo 'нет такого класса! ' . "<br>" . $a . "<br>" . 'По адресу: ' . "<br>" . $b;
             exit;
