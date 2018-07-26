43cd21d54758a755be4cc7b75f020812329632b9
Aleksandr-Borodin <aleksandrov477@gmail.com>
2018-07-27 00:02:48 +0300

    Исправлены слеши и мал. на бол. букву. Пора использовать директори сепаратор)

--- modules/bootstrap.php
+++ modules/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 // Автозагрузка классво через ооп;
-$path = __DIR__ . '\..\classes\load.php';
+$tpath = dirname(__DIR__);
+$path = $tpath . '/classes/Load.php';
 require_once $path;
 $loader = new Load();
 spl_autoload_register([$loader, 'loadclass']);
